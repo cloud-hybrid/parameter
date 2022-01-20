6816356ae5b78881e324bff506d419a2f5ddb7f8
Your Namegit <jacob.sanders@cloudhybrid.io>
2022-01-20 09:55:20 -0600

    Type Exports

--- src/parameter.ts
+++ src/parameter.ts
@@ -1,69 +1,5 @@
 import { Title, Lowercase, Case } from "./train-case";
 
-enum Type {
-    /// Directory type
-    Directory = "Directory",
-    /// Screaming-Train-Case string
-    Train = "Screaming-Train-Case",
-    /// Train-Case string
-    Dash = "Train-Case"
-}
-
-enum Properties {
-    /*** Number of Properties used with Parameter.default Constructor */
-    Default = 4,
-    /*** Number of Properties used when Negating the Identifier property, but inclusion of Provider */
-    Provider = 5,
-    /*** Number of Properties used when Negating the Provider property, but inclusion of Identifier */
-    Identifier = 5,
-    /*** Number of Properties used with a Full Parameter constructor call */
-    Parameter = 6
-}
-
-interface Options {
-    /***
-     * organization - Target Deliverable Maintainer
-     *
-     * @type {string}
-     */
-    organization: string;
-
-    /***
-     * environment - Network (L2) Seperated Alias
-     *
-     * @type {string}
-     */
-    environment: string;
-
-    /***
-     * application - Stack, Functional Purpose, or Common-Name
-     *
-     * @type {string}
-     */
-    application: string;
-
-    /***
-     * provider - Service(s) either Consumed or Provided
-     *
-     * @type {string}
-     */
-    resource: string;
-
-    /***
-     * resource - Descriptive Identifier
-     *
-     * @type {string}
-     */
-    provider: string | null;
-
-    /***
-     * identifier - Additional, Optional String
-     *
-     * @type {string}
-     */
-    identifier: string | null;
-}
-
 class Parameter implements Options {
     organization: string;
     environment: string;
@@ -206,6 +142,71 @@ class Parameter implements Options {
                 : (this.identifier && !this.provider) ? 5
                     : 4;
     }
+}
+
+
+export enum Type {
+    /// Directory type
+    Directory = "Directory",
+    /// Screaming-Train-Case string
+    Train = "Screaming-Train-Case",
+    /// Train-Case string
+    Dash = "Train-Case"
+}
+
+export enum Properties {
+    /*** Number of Properties used with Parameter.default Constructor */
+    Default = 4,
+    /*** Number of Properties used when Negating the Identifier property, but inclusion of Provider */
+    Provider = 5,
+    /*** Number of Properties used when Negating the Provider property, but inclusion of Identifier */
+    Identifier = 5,
+    /*** Number of Properties used with a Full Parameter constructor call */
+    Parameter = 6
+}
+
+export interface Options {
+    /***
+     * organization - Target Deliverable Maintainer
+     *
+     * @type {string}
+     */
+    organization: string;
+
+    /***
+     * environment - Network (L2) Seperated Alias
+     *
+     * @type {string}
+     */
+    environment: string;
+
+    /***
+     * application - Stack, Functional Purpose, or Common-Name
+     *
+     * @type {string}
+     */
+    application: string;
+
+    /***
+     * provider - Service(s) either Consumed or Provided
+     *
+     * @type {string}
+     */
+    resource: string;
+
+    /***
+     * resource - Descriptive Identifier
+     *
+     * @type {string}
+     */
+    provider: string | null;
+
+    /***
+     * identifier - Additional, Optional String
+     *
+     * @type {string}
+     */
+    identifier: string | null;
 }
 
 export { Parameter };
